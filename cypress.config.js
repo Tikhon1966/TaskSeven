7ab13a9902063d76611b24581981e6174aecd8a4
Oleg Tikhomirov <ovt1966@gmail.com>
2023-11-10 21:12:35 +0300

    Add CypressCloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'ekk2an',
   e2e: {
     baseUrl: "http://qamid.tmweb.ru/",
     setupNodeEvents(on, config) {

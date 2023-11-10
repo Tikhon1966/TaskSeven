2db19a4c0807dd0284228a6d492c5b46b8f6e3dc
Oleg Tikhomirov <ovt1966@gmail.com>
2023-11-10 18:52:26 +0300

    Add 3 test

--- cypress/e2e/afisha.cy.js
+++ cypress/e2e/afisha.cy.js
@@ -1,11 +1,12 @@
+import data from "../fixtures/data";
+
 describe('movie tickets reservation screen', () => {
    beforeEach(() => {
      cy.visit('/');
    })
   it('should display 7 days', () => {
     cy.visit("/")
-    cy.get(".page-header__title").should("be.visible");
-    cy.get(".page-nav__day").should("have.length", 7);
-   // cy.get('.page-nav__day').should('have.length', 7)
+    cy.get(data.selectors.titlePageCinema).should("be.visible");
+    cy.get(data.selectors.days).should("have.length", 7);
   })
 })
---
+++ cypress/e2e/booking.cy.js
@@ -0,0 +1,39 @@
+import data from "../fixtures/data";
+
+describe("booking tickets on movie", () => {
+  it("should book a movie in an available hall", () => {
+    cy.visit("/admin")
+    cy.login(data.emailValid, data.passwordValid)
+
+    cy.fixture("data").as("data")
+    cy.get("@data").then((data) => {
+      const expectedHallName = data.hallNames[3]
+      cy.get("#hall-control ul > li")
+        .should("have.length", data.hallNames.length)
+        .each((element, index) => {
+          cy.wrap(element)
+            .invoke("text")
+            .then((text) => {
+              if (text.includes(expectedHallName)) {
+                cy.log(
+                  `Element contains the expected hall: ${expectedHallName}`
+                );
+                expect(text).to.equal(expectedHallName)
+              }
+            })
+        })
+    })
+    cy.visit("/")
+    cy.get(data.selectors.days).eq(3).click()
+    cy.get(".movie").last().contains("12:00").click()
+    cy.fixture("seats").then((seats) => {
+      seats.forEach((seat) => {
+        cy.get(
+          `.buying-scheme__wrapper > :nth-child(${seat.row}) > :nth-child(${seat.seat})`
+        ).click()
+      })
+    })
+    cy.contains(data.ButtonBookingName).click()
+    cy.contains(data.BookingResult).should("be.visible")
+  })
+})
